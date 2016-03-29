8cdfd5b6866f690e7b6ab2ba4ca9a0993010efb9
Jaron Patena <patenaj@coyote.csusb.edu>
2016-03-29 11:42:06 -0700

    initial commit

---
+++ LOC.cpp
@@ -0,0 +1,35 @@
+// Jaron Patena
+// LOC.cpp
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Counter
+{
+	private:
+		int numlines; // number of lines
+		string line; // input string
+	public:
+		void linesofcode();
+};
+
+void Counter::linesofcode() // counts LOC
+{
+	numlines = 0;
+	while (cin.good())
+	{
+		getline(cin, line);
+// if comment found or empty line skip to next iteration
+		if(line.find("//") == 0 || line.empty() == 1) continue;
+		numlines += 1;
+	}	
+	cout << "Total LOC: " << numlines << endl;
+}
+
+int main()
+{ 
+	Counter count;
+	count.linesofcode();
+	return 0;
+}
